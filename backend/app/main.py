70dc358021e1e5ad5a00b9573398f3da978fead4
prateeksingh168 <prateeksingh420420@gmail.com>
2026-04-27 18:51:46 +0530

    final QR + scan system

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.database import Base, engine
 from app.routes.visitor import router as visitor_router
 from app.routes.visit import router as visit_router
 from app.routes.dashboard import router as dashboard_router
 from app.routes.auth import router as auth_router
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-import os
-from app.models import user, visitor, visit, qr
 
 Base.metadata.create_all(bind=engine)
 
@@ -20,18 +18,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-QR_PATH = os.path.join(BASE_DIR, "qr_codes")
-
-os.makedirs(QR_PATH, exist_ok=True)
-
-app.mount("/qr_codes", StaticFiles(directory=QR_PATH), name="qr")
 
 app.include_router(auth_router)
 app.include_router(visitor_router)
 app.include_router(visit_router)
 app.include_router(dashboard_router)
-app.mount("/qr_codes", StaticFiles(directory="qr_codes"), name="qr")
 
 @app.get("/")
 def root():
