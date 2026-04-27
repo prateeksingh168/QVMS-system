638617f51568c6a899f81546a8bc41ad0413e1d9
prateeksingh168 <prateeksingh420420@gmail.com>
2026-04-27 12:28:47 +0530

    fix: algorithm fix

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -15,4 +15,5 @@ def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    print("ALGO:", settings.ALGORITHM)
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm="HS256")
